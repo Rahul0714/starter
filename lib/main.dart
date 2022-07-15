f6580a0fafdff4ec4c4e1146a76a1676457aa6d6
Rahul Mujumdar <rahulmujumdar2000@gmail.com>
2022-07-15 12:20:54 +0530

    ch#4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -14,34 +14,47 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Awesome App"),
+      appBar: AppBar(
+        title: const Text("Awesome App"),
+      ),
+      body: Center(
+        child: Container(
+          color: Colors.teal,
+          width: 100,
+          height: 100,
         ),
-        body: Container(
-          color: Colors.green[100],
-          width: double.infinity,
-          height: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                width: 100,
-                height: 100,
-                color: Colors.red,
-              ),
-              Container(
-                width: 100,
-                height: 100,
-                color: Colors.yellow,
-              ),
-              Container(
-                width: 100,
-                height: 100,
-                color: Colors.green,
-              ),
-            ],
+      ),
+      drawer: Drawer(
+        child: ListView(children: const [
+          UserAccountsDrawerHeader(
+            accountName: Text("Rahul Mujumdar"),
+            accountEmail: Text("rahul@mail.com"),
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  "https://images.unsplash.com/photo-1564648351416-3eec9f3e85de?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8a29zdGVubG9zZSUyMGJpbGRlcnxlbnwwfHwwfHw%3D&w=1000&q=80"),
+            ),
           ),
-        ));
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text("Rahul Mujumdar"),
+            subtitle: Text("Nothing"),
+            trailing: Icon(Icons.edit),
+          ),
+          ListTile(
+            leading: Icon(Icons.email),
+            title: Text("Email"),
+            subtitle: Text("rahul@mail.com"),
+            trailing: Icon(Icons.edit),
+          ),
+        ]),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(
+          Icons.edit,
+          color: Colors.white,
+        ),
+      ),
+    );
   }
 }
